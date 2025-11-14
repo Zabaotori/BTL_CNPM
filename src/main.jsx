1bf042223a121cd789d67d1437c5518452b63494
Bao Vo <bao.volam91105@hcmut.edu.vn>
2025-11-14 14:08:35 +0700

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { DatePicker } from 'antd';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className=''>Hello</h1>
   </StrictMode>,
 )
